311a9f3b083dd7155c9d9a01c05ec6a16f4d22d4
Saverio Delpriori <saverio.delpriori@uniurb.it>
2018-10-03 17:51:19 +0200

    Better testing routine

--- test.sh
+++ test.sh
@@ -1,23 +1,60 @@
 #!/usr/bin/env bash
 
- echo -e "\n\n\npy.exe exporter.py -l 10\n"
- py.exe exporter.py -l 10
- echo -e "\n\n\npy.exe exporter.py -d 10\n"
- py.exe exporter.py -d 10
- echo -e "\n\n\npy.exe exporter.py -g 12.92290593\n"
- py.exe exporter.py -g 12.92290593
- echo -e "\n\n\npy.exe exporter.py --longitude 12.92290593 --latitude 43.74830223\n"
- py.exe exporter.py --longitude 12.92290593 --latitude 43.74830223
- echo -e "\n\n\npy.exe exporter.py -l 10 -m -o test.csv\n"
- py.exe exporter.py -l 10 -m -o test.csv
- echo -e "\n\n\npy.exe exporter.py -t 16 -m -o test_track.csv\n"
- py.exe exporter.py -T 16 -m -o test_track.csv
+function assert {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "Error with $1" >&2
+        exit;
+    else
+        echo "Test passed"
+    fi
+    return $status
+}
 
- echo -e "\n\n\npy.exe exporter.py -a -m\n"
- py.exe exporter.py -a -m
- echo -e "\n\n\npy.exe exporter.py -a -t 16\n"
- py.exe exporter.py -a -T 16
- echo -e "\n\n\npy.exe exporter.py -a -l 10\n"
- py.exe exporter.py -a -l 10
- echo -e "\n\n\npy.exe exporter.py --longitude 12.92290593 --latitude 43.74830223 -d 1000 -l 100\n"
- py.exe exporter.py -a --longitude 12.92290593 --latitude 43.74830223 -d 1000 -l 100
+function assert_not {
+    "$@"
+    local status=$?
+    if [ $status -eq 0 ]; then
+        echo "Error with $1" >&2
+        exit;
+    else
+        echo "Test passed"
+    fi
+    return $status
+}
+
+
+# Weekly raw data table
+echo -e "\n\n\npy.exe exporter.py -l 10\n"
+assert py.exe exporter.py -l 10
+echo -e "\n\n\npy.exe exporter.py -d 10\n"
+assert py.exe exporter.py -d 10
+echo -e "\n\n\npy.exe exporter.py -l 10 --debug \n"
+assert py.exe exporter.py -l 10 --debug
+echo -e "\n\n\npy.exe exporter.py -l 10 -d \n"
+assert_not py.exe exporter.py -l 10 -d
+echo -e "\n\n\npy.exe exporter.py -g 12.92290593\n"
+assert py.exe exporter.py -g 12.92290593
+echo -e "\n\n\npy.exe exporter.py --longitude 12.92290593 --latitude 43.74830223\n"
+assert py.exe exporter.py --longitude 12.92290593 --latitude 43.74830223
+echo -e "\n\n\npy.exe exporter.py -l 10 -m -o test.csv\n"
+assert py.exe exporter.py -l 10 -m -o test.csv
+echo -e "\n\n\npy.exe exporter.py -T 16 -m -o test_track.csv\n"
+assert py.exe exporter.py -T 16 -m -o test_track.csv
+
+# Old raw data table
+echo -e "\n\n\npy.exe exporter.py -O -A 2018-04-01 --before 2018-04-02\n"
+assert py.exe exporter.py -O -A 2018-04-01 --before 2018-04-02
+
+# Aggregate table
+echo -e "\n\n\npy.exe exporter.py -a -m\n"
+assert_not py.exe exporter.py -a -m
+echo -e "\n\n\npy.exe exporter.py -a -t 16\n"
+assert_not py.exe exporter.py -a -T 16
+echo -e "\n\n\npy.exe exporter.py -a -l 10\n"
+assert py.exe exporter.py -a -l 10
+echo -e "\n\n\npy.exe exporter.py --longitude 12.92290593 --latitude 43.74830223 -d 100 -l 10\n"
+assert py.exe exporter.py -a --longitude 12.92290593 --latitude 43.74830223 -d 100 -l 10
+
+
